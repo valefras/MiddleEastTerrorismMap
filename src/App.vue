31b74f8920d412617ece261e5a453c6e52b12d5f
valefras <48183554+valefras@users.noreply.github.com>
2023-07-04 15:03:21 +0200

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <router-view />
 </template>
-
+<script>
+import { onMounted } from "vue";
+export default {
+  setup() {
+    onMounted(() => {
+      document.title = "Terrorism in the Middle East Map";
+    });
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production",
   transpileDependencies: true,
   configureWebpack: {
     module: {
